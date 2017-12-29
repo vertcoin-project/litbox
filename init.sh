3a4ba8d914e5078989ee3b5c71e8aef521f8ac24
Gert-Jaap Glasbergen <gertjaap@decoscrypto.com>
2017-12-29 14:56:10 +0100

    Added init script and admin box

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+mkdir -p /tmp/vertcoin-box
+cd /tmp/vertcoin-box
+git clone --depth 1 https://github.com/gertjaap/vertcoin-box
+git clone --depth 1 https://github.com/gertjaap/vertcoin-box-tunnel
+git clone --depth 1 https://github.com/gertjaap/vertcoin-box-admin
+cd /tmp/vertcoin-box/vertcoin-box-admin
+ssh-keygen -t rsa -N "" -f ./vertcoin-box-admin.key
+docker build . -t vertcoin-box-admin
+
+cd /tmp/vertcoin-box/vertcoin-box-tunnel
+cp ../vertcoin-box-admin/vertcoin-box-admin.key .
+docker build . -t vertcoin-box-tunnel
+
+mkdir -p /usr/local/vertcoin-box
+cp -R /tmp/vertcoin-box/vertcoin-box/* /usr/local/vertcoin-box 
+mkdir -p /usr/local/vertcoin-box/data/lit
