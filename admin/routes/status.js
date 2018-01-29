648e953aeebd9fa800cc20606bcd313f0751f101
Gert-Jaap Glasbergen <gertjaap@Gert-Jaaps-MBP.decos.com>
2018-01-29 16:24:18 +0100

    Show hidden service hostname on status page

--- admin/routes/status.js
+++ admin/routes/status.js
@@ -1,25 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var ensure = require('connect-ensure-login');
+var fs = require('fs');
 var tunnel = require('../tunnel');
 var lit = require('../lit');
 
 router.get('/', ensure.ensureLoggedIn('/auth/login'), function(req, res, next) {
+    var torHost = fs.readFileSync('/root/tor/hostname');
     tunnel.checkTunnel((err, success) => {
         var ws = lit.connect();
         ws.on('open', () => {
             lit.getBalance(ws, (litErr, data) => {
                 if(data.result && data.error == null) {
-                    res.render('status', { litStatus: true, litError: null, tunnelStatus: success, tunnelError: err });
+                    res.render('status', { torHost: torHost, litStatus: true, litError: null, tunnelStatus: success, tunnelError: err });
                 } else {
-                    res.render('status', { litStatus: false, litError: data.error, tunnelStatus: success, tunnelError: err });
+                    res.render('status', { torHost: torHost, litStatus: false, litError: data.error, tunnelStatus: success, tunnelError: err });
                 }
             });
         });
         ws.on('error', (litErr) => {
             ws.close();
-            res.render('status', { litStatus: false, litError: JSON.stringify(litErr), tunnelStatus: success, tunnelError: err });
-            
+            res.render('status', { torHost: torHost, litStatus: false, litError: JSON.stringify(litErr), tunnelStatus: success, tunnelError: err });
         });
     });
 });
