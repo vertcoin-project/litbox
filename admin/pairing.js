31400e9e8663352ce8b3cd1f42fd842cc9638fd5
Gert-Jaap Glasbergen <g.glasbergen@decos.com>
2018-02-07 19:41:38 +0100

    Updated pairing code

--- admin/pairing.js
+++ admin/pairing.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var crypto = require('crypto');
 var bodyParser = require('body-parser');
-
 var app = express();
-app.use(require('body-parser').json());
+
+app.use(bodyParser.json());
 
 app.post('/pair', function(req, res, next) {
-    console.log("Pairing request received");
+    const signature = req.headers["x-hmac-signature"];
+    const body = req.body;
+
+    var testSignature = crypto.createHmac("sha256", "abc123").update(JSON.stringify(body)).digest("base64")
+
+    console.log("Pairing request received. Signature: [", signature , "] [", testSignature ,"] \r\n\r\n Request: \r\n" , body);
+
+
     res.sendStatus(201);
 });
 
